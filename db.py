436f51b87c818b546b326a207f5953b601470729
Eric Chen <ericchen314@gmail.com>
2023-08-09 11:57:12 -0700

    Add new field for filename reference

--- db.py
+++ db.py
@@ -9,16 +9,17 @@ def create_db() -> None:
                       id INTEGER PRIMARY KEY AUTOINCREMENT,
                       prompt TEXT,
                       answer TEXT,
-                      citations TEXT
+                      citations TEXT,
+                      file_path TEXT
                   )''')
   conn.close()
 
-def log_to_database(prompt: str, answer: str, citations: list) -> None:
+def log_to_database(prompt: str, answer: str, citations: list, file_path: str) -> None:
     conn = sqlite3.connect('prompt_logs.db')
     c = conn.cursor()
 
     str_citation = json.dumps(citations)
-    c.execute('INSERT INTO prompt_logs (prompt, answer, citations) VALUES (?, ?, ?)', (prompt, answer, str_citation))
+    c.execute('INSERT INTO prompt_logs (prompt, answer, citations, file_path) VALUES (?, ?, ?, ?)', (prompt, answer, str_citation, file_path))
 
     conn.commit()
     conn.close()
